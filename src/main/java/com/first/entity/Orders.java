087f67a31da8a57b5b8a9a24ea25a0380858454d
K-tech660 <148431685+K-tech660@users.noreply.github.com>
2024-04-15 18:37:15 +0530

    Update Orders.java

--- src/main/java/com/first/entity/Orders.java
+++ src/main/java/com/first/entity/Orders.java
@@ -3,14 +3,10 @@ package com.first.entity;
 
 
 import java.util.Date;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
-
-
 @Entity
 public class Orders {
 
